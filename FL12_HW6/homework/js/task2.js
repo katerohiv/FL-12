3141c9a338178f740d67d0dc53b8fbb7b4bf7851
Kateryna Rohiv <51263644+katerohiv@users.noreply.github.com>
2019-12-23 23:00:48 +0200

    implement task 2

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,17 @@
-// Your code goes here
+const a = parseInt(prompt('Please enter value a'));
+const b = parseInt(prompt('Please enter value b'));
+const c = parseInt(prompt('Please enter value c'));
+const zero = 0;
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    alert(`input values should be ONLY numbers`)
+} else if (a === zero || b === zero || c === zero) {
+    alert(`A triangle must have 3 sides with a positive definite length`)
+} else if (a + b < c || a + c < b || b + c < a) {
+    console.log(`Triangle doesn’t exist`)
+} else if (a === b && b === c && a === c) {
+    console.log(`Equilateral triangle`)
+} else if (a === b && a !== c && c !== b || a === c && a !== b && c !== b || b === c && b !== a && c !== a) {
+    console.log(`Isosceles triangle`)
+} else if (a !== b && a !== c && b !== c) {
+    console.log(`Scalene triangle`)
+}
