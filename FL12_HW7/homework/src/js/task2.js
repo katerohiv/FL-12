0879f98670eabaef3f90a50912573983a5e35d4c
Kateryna Rohiv <51263644+katerohiv@users.noreply.github.com>
2019-12-30 22:05:54 +0200

    Complete task 2

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,55 @@
-// Your code goes here
+const totalAttempts = 3;
+const startWin = 100;
+const minRangeEnd = 8;
+const rangeStart = 0;
+let rangeEnd = minRangeEnd;
+let firstAttempt = 0;
+let maxWin = startWin;
+let currentPrize = maxWin;
+let totalWin = 0;
+let correction = 1;
+let areYouReady = confirm('Do you want to play a game?');
+while (areYouReady) {
+    while (areYouReady && firstAttempt < totalAttempts) {
+        let randomNum = Math.floor(Math.random() * (rangeEnd + correction));
+        let userNumber = prompt(`Please enter the number from ${rangeStart} to ${rangeEnd}
+Attempts left: ${totalAttempts - firstAttempt}
+Total prize: ${totalWin}$
+Possible prize on current attempt: ${currentPrize}$`).trim();
+        if (Number(userNumber) === randomNum) {
+            totalWin += currentPrize;
+            maxWin = maxWin * 2;
+            currentPrize = maxWin;
+            areYouReady = confirm(`Congrats! You're prize is ${totalWin}$. Do you want to to continue?`);
+            firstAttempt = 0;
+            rangeEnd = rangeEnd + 4;
+            randomNum = Math.floor(Math.random() * (rangeEnd + correction));
+            console.log(rangeEnd, correction);
+        } else if (!userNumber) {
+            alert(`Thank you for a game. Your prize is: ${totalWin}`);
+            areYouReady = confirm('Do you want to play again?');
+            totalWin = 0;
+            firstAttempt = 0;
+            maxWin = startWin;
+            currentPrize = maxWin;
+            rangeEnd = minRangeEnd;
+        } else {
+            firstAttempt++;
+            currentPrize = Math.floor(currentPrize / 2);
+
+        }
+    }
+    if (firstAttempt >= totalAttempts) {
+        alert(`Thank you for a game. Your prize is: ${totalWin}`);
+        areYouReady = confirm('Do you want to play again?');
+        totalWin = 0;
+        firstAttempt = 0;
+        maxWin = startWin;
+        currentPrize = maxWin;
+        rangeEnd = minRangeEnd;
+    }
+}
+
+if (!areYouReady) {
+    alert('You did not become a millionaire, but can.');
+}
